77756b24d4da3bbd3b5570b434ea5b6e621ea273
SebSept <sebastienmonterisi@yahoo.fr>
2020-11-15 21:52:14 +0100

    nom du module cohérent avec depot.

--- fop_example_module.php
+++ fop_example_module.php
@@ -21,7 +21,7 @@ declare(strict_types=1);
 /**
  * Class exampleModule
  */
-class exampleModule extends Module
+class fop_example_module extends Module
 {
     public function install(): bool
     {
