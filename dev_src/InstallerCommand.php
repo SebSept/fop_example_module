546eed76126d71307dd04dddff75a679e78581fb
SebSept <sebastienmonterisi@yahoo.fr>
2020-11-20 22:52:03 +0100

    wip tests

--- dev_src/InstallerCommand.php
+++ dev_src/InstallerCommand.php
@@ -15,7 +15,6 @@ namespace FriendsOfPresta\BaseModuleInstaller;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Console\Question\Question;
 use Symfony\Component\Filesystem\Filesystem;
 
 class InstallerCommand extends Command
@@ -39,16 +38,20 @@ class InstallerCommand extends Command
     {
         $this->input = $input;
         $this->output = $output;
-        $this->welcomeMessage();
+        try {
+            $this->displayWelcomeMessage();
+            $this->copyConfigurationFiles(!$input->getOption('no-interaction'));
 
-        $this->insertComposerScripts(!$input->getOption('no-interaction'));
-        $this->copyConfiguration(!$input->getOption('no-interaction'));
-        $output->writeln('hello');
+            return Command::SUCCESS; /* @phpstan-ignore-line */
+        } catch (\Exception $exception) {
+            // @todo formatage d'erreur
+            $this->output->writeln('Installation aborted : ' . $exception->getMessage(), OutputInterface::VERBOSITY_QUIET);
 
-        return Command::SUCCESS; /* @phpstan-ignore-line */
+            return Command::FAILURE; /* @phpstan-ignore-line */
+        }
     }
 
-    private function welcomeMessage(): void
+    private function displayWelcomeMessage(): void
     {
         $formatter = $this->getHelper('formatter');
         $welcome_text = $formatter->formatBlock('Welcome to Friends Of Presta Module development tools installer.', 'info');
@@ -56,69 +59,35 @@ class InstallerCommand extends Command
         $this->output->writeln(['No files will be erased.', 'Modified or replaced files are copied using the suffix .backup.\<timestamp>']);
     }
 
-    /**
-     * Insert Tools scripts into composer.json
-     *
-     * @param bool $interaction
-     */
-    private function insertComposerScripts(bool $interaction): void
+    private function copyConfigurationFiles(bool $interaction = false): void
     {
-        // composer du projet dans lequel la commande est lancée : la cible
-        $project_composer_path = __DIR__ . '/../../../../composer.json';
-        // composer du package actuel : la source
-        $fop_package_composer_path = __DIR__ . '/../composer.json';
+        $this->output->writeln('Begining copying configuration files...', OutputInterface::VERBOSITY_VERBOSE);
         $fs = new Filesystem();
-        $questioner = $this->getHelper('question');
-
-        // backup composer
-        $question = new Question('Insert tools in composer.json ? (yes)', 'yes');
-        if (!$interaction || 'yes' === $questioner->ask($this->input, $this->output, $question)) {
-            if (!realpath($project_composer_path)) {
-                $this->output->writeln($fs->makePathRelative($project_composer_path, __DIR__));
-                throw new \Exception('Source file not found : ' . $project_composer_path);
-            }
-
-            $composer_backup = $project_composer_path . 'backup.' . date('U');
-            $fs->copy($project_composer_path, $composer_backup);
-            $this->output->writeln("composer.json backup created ($composer_backup)", OutputInterface::VERBOSITY_QUIET);
-        } else {
-            $this->output->writeln('composer.json not backed up.', OutputInterface::VERBOSITY_VERBOSE);
+        foreach (self::CONFIG_TO_COPY as $spl_file) {
+            $this->copySplFile(new \SplFileInfo(__DIR__ . '/../' . $spl_file));
         }
-
-        trigger_error('implement the rest : see composer/JsonManipulator');
-
-        return;
-        // extraction composer du package
-        $fop_package_composer_json = json_decode(file_get_contents($fop_package_composer_path));
-        if (false === $fop_package_composer_json) {
-            throw new Exception('Pas possible de decoder ' . $fop_package_composer_path);
-        }
-
-        // extraction composer cible
-        $project_composer_json = json_decode(file_get_contents($project_composer_path));
-        if (false === $project_composer_json) {
-            throw new Exception('Pas possible de decoder ' . $project_composer_path);
-        }
-
-        // insertion des scripts
-        foreach ((array) $fop_package_composer_json->scripts as $script_name => $script_command) {
-            $project_composer_json->scripts->$script_name = $script_command;
-        }
-
-        // enregistrement du nouveau composer.json
-        $new_composer_content = json_encode(($project_composer_json));
-        file_put_contents($project_composer_path . '.test.json', $new_composer_content);
     }
 
-    private function copyConfiguration(bool $interaction = false): void
+    private function copySplFile(\SplFileInfo $file_info)
     {
-        throw new \Exception('Not implemented');
+        if (!$file_info->isFile()) {
+            \dump($file_info);
+            throw new \Exception('oops ' . $file_info->getFilename()); // @todo oops
+        }
+
+        if (!$file_info->isDir()) {
+            // @todo ecrire fonction de copie
+            $this->output->writeln('copie du fichier ' . $file_info->getFilename());
+
+            return;
+        }
+
+        $this->output->writeln('copie du dossier par récursion ...' . $file_info->getFilename());
+        foreach ($file_info as $sub_file_info) {
+            $this->output->writeln('sub : ' . $sub_file_info->getFilename());
+
+            // @todo ecrire fonction de création de dossier
+            $this->copySplFile($sub_file_info);
+        }
     }
 }
-
-return;
-
-insert_composer_scripts(); // déclarer les bin dans le bin composer : a faire dans composer
-copy_config();
-
-// copier les config avec demande de confirmation  ou création auto de backup
