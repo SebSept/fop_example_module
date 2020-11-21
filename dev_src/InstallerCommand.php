a10eae09f157cd4432b7a41665730775da8d55c9
SebSept <sebastienmonterisi@yahoo.fr>
2020-11-21 22:12:07 +0100

    installeur insertion scripts composer.

--- dev_src/InstallerCommand.php
+++ dev_src/InstallerCommand.php
@@ -4,22 +4,23 @@
  *
  * @author Sébastien Monterisi <contact@seb7.fr>
  */
-// c'est du brutal ici !
-// @todo copier les config (avec filesystem)
 // @todo faire une commande symfony (pour gérer des options et gerer des questions)
-// @todo utiliser filesystem pour lancer des exceptions.
 // @todo linter le composer.json final au besoin.
 
 namespace FriendsOfPresta\BaseModuleInstaller;
 
+use Composer\Json\JsonManipulator;
+use http\Exception\RuntimeException;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Question\Question;
 use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\Process\Process;
 
-class InstallerCommand extends Command
+final class InstallerCommand extends Command
 {
-    const CONFIG_TO_COPY = ['.github/workflows/', '.php_cs', 'grumphp.yml.dist', 'phpstan.neon.dist'];
+    const CONFIG_TO_COPY = ['.github/workflows/', '.php_cs.dist', 'grumphp.yml.dist', 'phpstan.neon.dist'];
     /**
      * @var OutputInterface
      */
@@ -46,12 +47,14 @@ class InstallerCommand extends Command
         $this->output = $output;
         try {
             $this->displayWelcomeMessage();
-            $this->copyConfigurationFiles(!$input->getOption('no-interaction'));
+            trigger_error('reactiver ces étapes');
+//            $this->copyConfigurationFiles(!$input->getOption('no-interaction'));
+//            $this->initGrumphp();
+            $this->insertComposerScripts(!$input->getOption('no-interaction'));
 
             return Command::SUCCESS; /* @phpstan-ignore-line */
         } catch (\Exception $exception) {
-            // @todo formatage d'erreur
-            $this->output->writeln('Installation aborted : ' . $exception->getMessage(), OutputInterface::VERBOSITY_QUIET);
+            $this->output->writeln('<fg=red>Installation aborted : ' . $exception->getMessage() . '<fg/>', OutputInterface::VERBOSITY_QUIET);
 
             return Command::FAILURE; /* @phpstan-ignore-line */
         }
@@ -110,5 +113,61 @@ class InstallerCommand extends Command
     private function destinationPath(\SplFileInfo $source): string
     {
         return '../../../../' . rtrim($this->fs->makePathRelative($source->getPathname(), __DIR__ . '/../'), '/');
+    }
+
+    private function initGrumphp()
+    {
+        $this->output->write('Intalling grumphp : ');
+        // launch throught process. (any idea how to simply call the GrumPHP\Console\Command\Git\InitCommand() ?
+        $process = new Process(['./vendor/bin/grumphp', 'git:init'], __DIR__ . '/../../../../');
+        $process->run();
+        if (!$process->isSuccessful()) {
+            throw new \Exception('Error running grumphp git:init : ' . $process->getErrorOutput());
+        }
+        $this->output->writeln($process->getOutput());
+    }
+
+    private function insertComposerScripts(bool $interaction): void
+    {
+        $target_composer_path = __DIR__ . '/../../../../composer.json';
+        $target_composer_path_backup = sprintf('%s/../../../../composer.%s.json', __DIR__, date('U'));
+        $source_composer_path = __DIR__ . '/../composer.json';
+        if (!realpath($target_composer_path)) {
+            throw new \LogicException(__FUNCTION__ . ' : Target composer not found at ' . $target_composer_path);
+        }
+        if (!realpath($source_composer_path)) {
+            throw new \LogicException(__FUNCTION__ . ' : Source composer not found at ' . $source_composer_path);
+        }
+
+        $fs = new Filesystem();
+        $questioner = $this->getHelper('question');
+
+        // backup composer
+        $question = new Question('Insert tools\' scripts in composer.json ? (yes)', 'yes');
+        if (!$interaction || 'yes' === $questioner->ask($this->input, $this->output, $question)) {
+            $fs->copy($target_composer_path, $target_composer_path_backup);
+            $this->output->writeln("composer.json backup created : $target_composer_path", OutputInterface::VERBOSITY_QUIET);
+        } else {
+            $this->output->writeln('composer.json not backed up.', OutputInterface::VERBOSITY_VERBOSE);
+        }
+
+        $source_scripts = json_decode(file_get_contents($source_composer_path)); // todo use symfony (de)serializer ?
+        if (false === $source_scripts) {
+            throw new \RuntimeException('Failed to jsondecode ' . $source_composer_path);
+        }
+
+        // extraction composer cible
+//        $project_composer_json = json_decode(file_get_contents($target_composer_path));
+//        if (false === $project_composer_json) {
+//            throw new RuntimeException('Failed to jsondecode ' . $target_composer_path);
+//        }
+        $json_manipulator = new JsonManipulator(file_get_contents($target_composer_path));
+
+        // insertion des scripts
+        foreach ((array) $source_scripts->scripts as $script_name => $script_command) {
+            $json_manipulator->addSubNode('scripts', $script_name, $script_command);
+        }
+
+        $fs->dumpFile($target_composer_path, $json_manipulator->getContents());
     }
 }
